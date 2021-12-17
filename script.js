8bd666d94d5efeb1b9ea1b69e7e66657f9a614de
danielp345 <danielp345@gmail.com>
2021-12-17 22:20:38 +0100

    ready to use

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const incomeSection = document.querySelector(".income-area");
 const expensesSection = document.querySelector(".expenses-area");
-const availableMoney = document.querySelector(".available-moeny");
+const availableMoney = document.querySelector(".available-money");
 const addTransactionPanel = document.querySelector(".add-transaction-panel");
 
 const nameInput = document.querySelector("#name");
@@ -19,7 +19,7 @@ let root = document.documentElement;
 let ID = 0;
 let categoryIcon;
 let selectedCategory;
-let moenyArray = [0];
+let moneyArray = [0];
 
 const showPanel = () => {
 	addTransactionPanel.style.display = "flex";
@@ -67,8 +67,9 @@ const createNewTransaction = () => {
 		: expensesSection.appendChild(newTransaction) &&
 		  newTransaction.classList.add("expense");
 
-	moenyArray.push(parseFloat(amountInput.value));
+	moneyArray.push(parseFloat(amountInput.value));
 
+	countMoney(moneyArray);
 	closePanel();
 	ID++;
 	clearInputs();
@@ -78,8 +79,8 @@ const selectCategory = () => {
 	selectedCategory = categoryInput.options[categoryInput.selectedIndex].text;
 };
 
-const checkCategory = (transaction) => {
-	switch (transaction) {
+const checkCategory = (category) => {
+	switch (category) {
 		case "[ + ] Przychód":
 			categoryIcon = `<i class="fas fa-money-bill-wave"></i>`;
 			break;
@@ -98,6 +99,50 @@ const checkCategory = (transaction) => {
 	}
 };
 
+const countMoney = (money) => {
+	const newMoney = money.reduce((a, b) => a + b);
+	availableMoney.textContent = `${newMoney}zł`;
+};
+
+const deleteTransaction = (id) => {
+	const transactionToDelete = document.getElementById(id);
+
+	const transactionAmount = parseFloat(
+		transactionToDelete.childNodes[3].innerText
+	);
+
+	const indexOfTransaction = moneyArray.indexOf(transactionAmount);
+	moneyArray.splice(indexOfTransaction, 1);
+
+	// transactionToDelete.classList.contains('income') ? incomeSection.removeChild(transactionToDelete) : expensesSection.removeChild(transactionToDelete)
+	//
+
+	countMoney(moneyArray);
+	transactionToDelete.remove();
+};
+
+const deleteAllTransactions = () => {
+	incomeSection.innerHTML = "<h3>Przychód</h3>";
+	expensesSection.innerHTML = " <h3>Wydatki:</h3>";
+	availableMoney.textContent = "0zł";
+	moneyArray = [0];
+};
+
+const changeStyleToLight = () => {
+	root.style.setProperty("--first-color", " #f9f9f9");
+	root.style.setProperty("--second-color", " #14161f");
+	root.style.setProperty("--border-color", " rgba(0, 0, 0, 0.2)");
+};
+
+const changeStyleToDark = () => {
+	root.style.setProperty("--first-color", " #14161f");
+	root.style.setProperty("--second-color", " #f9f9f9");
+	root.style.setProperty("--border-color", " rgba(255, 255, 255, 0.4)");
+};
+
 addTransactionBtn.addEventListener("click", showPanel);
 cancelBtn.addEventListener("click", closePanel);
 saveBtn.addEventListener("click", checkForm);
+deleteAllBtn.addEventListener("click", deleteAllTransactions);
+lightStyleBtn.addEventListener("click", changeStyleToLight);
+darkStyleBtn.addEventListener("click", changeStyleToDark);
